1102a8371883a653d4c0d14f65a26e402f74a592
mshanemc <shane.mclaughlin@salesforce.com>
2020-06-11 16:17:19 -0500

    unscrew the test data, with geo now

--- README.md
+++ README.md
@@ -11,6 +11,11 @@ To work on this project in a scratch org:
 3. You can now use sfdx commands like `org:open`, push, pull, etc.
 4. After step 2, you can run `cci task run run_tests --org dev` to run all the apex tests
 
+## Sample Data
+
+1. Put data in the org however you like
+2. Run `cci task run extract_dataset`
+
 ## Troubleshooting
 
 If the default org doesn't show up down in the vscode tray, you can run `cci org default dev` to manually set the default org
--- datasets/sample.sql
+++ datasets/sample.sql
@@ -43,6 +43,8 @@ CREATE TABLE "FeedItem" (
 	parent_id VARCHAR(255), 
 	PRIMARY KEY (sf_id)
 );
+INSERT INTO "FeedItem" VALUES('0D51700000d6DgSCAU','<p>Here is another comment</p>','TextPost','a041700000K472BAAR');
+INSERT INTO "FeedItem" VALUES('0D51700000d6DgNCAU','<p>This one is my favorite</p>','TextPost','a041700000K472BAAR');
 CREATE TABLE "Open_Hours__c" (
 	sf_id VARCHAR(255) NOT NULL, 
 	"End_Time__c" VARCHAR(255), 
@@ -51,6 +53,10 @@ CREATE TABLE "Open_Hours__c" (
 	service__c VARCHAR(255), 
 	PRIMARY KEY (sf_id)
 );
+INSERT INTO "Open_Hours__c" VALUES('a011700000AvZ0RAAV','17:00:00.000Z','09:00:00.000Z','Thursday','a041700000K472CAAR');
+INSERT INTO "Open_Hours__c" VALUES('a011700000AvZ0SAAV','17:00:00.000Z','09:00:00.000Z','Tuesday','a041700000K472CAAR');
+INSERT INTO "Open_Hours__c" VALUES('a011700000AvZ0TAAV','17:00:00.000Z','09:00:00.000Z','Monday','a041700000K472CAAR');
+INSERT INTO "Open_Hours__c" VALUES('a011700000AvZ0UAAV','17:00:00.000Z','09:00:00.000Z','Wednesday','a041700000K472CAAR');
 CREATE TABLE "Referral__c" (
 	sf_id VARCHAR(255) NOT NULL, 
 	"Preferred_Channel__c" VARCHAR(255), 
@@ -59,12 +65,16 @@ CREATE TABLE "Referral__c" (
 	service__c VARCHAR(255), 
 	PRIMARY KEY (sf_id)
 );
+INSERT INTO "Referral__c" VALUES('a021700000I5C9OAAV','SMS','','0033B00000TxhXJQAZ','a041700000K472CAAR');
+INSERT INTO "Referral__c" VALUES('a021700000I5C9PAAV','Email','','0033B00000TxhXIQAZ','a041700000K472BAAR');
 CREATE TABLE "Service_Exclusion__c" (
 	sf_id VARCHAR(255) NOT NULL, 
 	contact__c VARCHAR(255), 
 	service__c VARCHAR(255), 
 	PRIMARY KEY (sf_id)
 );
+INSERT INTO "Service_Exclusion__c" VALUES('a031700000F9CrTAAV','','a041700000K472EAAR');
+INSERT INTO "Service_Exclusion__c" VALUES('a031700000F9CrUAAV','00317000012R0kxAAC','a041700000K472DAAR');
 CREATE TABLE "Service__c" (
 	sf_id VARCHAR(255) NOT NULL, 
 	"Name" VARCHAR(255), 
@@ -84,6 +94,12 @@ CREATE TABLE "Service__c" (
 	account__c VARCHAR(255), 
 	PRIMARY KEY (sf_id)
 );
+INSERT INTO "Service__c" VALUES('a041700000K472BAAR','Test Service for Adults','','18.0','San Francisco','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut efficitur lacus in aliquet dignissim. Cras molestie risus vel magna gravida congue facilisis non nisl. Maecenas laoreet est libero, vitae cursus justo posuere in. Nunc vel elit sapien. Integer velit augue, volutpat ut mattis et, lacinia at orci.','555 555 5555','2400 Clay','Food','www.google.com','94102','','false', '37.7897', '-122.397', '00117000016QYa8AAG');
+INSERT INTO "Service__c" VALUES('a041700000K472CAAR','Test Service for Teens','17.0','13.0','San Francisco','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut efficitur lacus in aliquet dignissim. Cras molestie risus vel magna gravida congue facilisis non nisl. Maecenas laoreet est libero, vitae cursus justo posuere in. Nunc vel elit sapien. Integer velit augue, volutpat ut mattis et, lacinia at orci.','555 555 5555','2400 Clay','Education','www.google.com','94102','','false', '37.7897', '-122.397', '00117000016QYa8AAG');
+INSERT INTO "Service__c" VALUES('a041700000K472DAAR','hidden service (only hidden from Shane)','','18.0','San Francisco','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut efficitur lacus in aliquet dignissim. Cras molestie risus vel magna gravida congue facilisis non nisl. Maecenas laoreet est libero, vitae cursus justo posuere in. Nunc vel elit sapien. Integer velit augue, volutpat ut mattis et, lacinia at orci.','555 555 5555','2400 Clay','Health','www.google.com','94102','','false', '37.7897', '-122.397', '00117000016QYa9AAG');
+INSERT INTO "Service__c" VALUES('a041700000K472EAAR','hidden service (all contacts)','','18.0','San Francisco','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut efficitur lacus in aliquet dignissim. Cras molestie risus vel magna gravida congue facilisis non nisl. Maecenas laoreet est libero, vitae cursus justo posuere in. Nunc vel elit sapien. Integer velit augue, volutpat ut mattis et, lacinia at orci.','555 555 5555','2400 Clay','Housing','www.google.com','94102','','false', '37.7897', '-122.397', '00117000016QYa7AAG');
+INSERT INTO "Service__c" VALUES('a041700000K472FAAR','far service','','18.0','San Francisco','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut efficitur lacus in aliquet dignissim. Cras molestie risus vel magna gravida congue facilisis non nisl. Maecenas laoreet est libero, vitae cursus justo posuere in. Nunc vel elit sapien. Integer velit augue, volutpat ut mattis et, lacinia at orci.','555 555 5555','2400 Clay','Housing','www.google.com','94102','','false', '2', '-2', '00117000016QYa7AAG');
+
 CREATE TABLE "Task" (
 	sf_id VARCHAR(255) NOT NULL, 
 	"Subject" VARCHAR(255), 
@@ -94,4 +110,7 @@ CREATE TABLE "Task" (
 	what_id VARCHAR(255), 
 	PRIMARY KEY (sf_id)
 );
+INSERT INTO "Task" VALUES('00T1700000IfdOoEAJ','Info to Client','','Completed','Normal','00317000012R0kxAAC','a021700000I5C9PAAV');
+INSERT INTO "Task" VALUES('00T1700000IfdOpEAJ','Reminder','','Not Started','Normal','00317000012R0kxAAC','a021700000I5C9PAAV');
+INSERT INTO "Task" VALUES('00T1700000IfdOqEAJ','Survey','','Not Started','Normal','00317000012R0kxAAC','a021700000I5C9PAAV');
 COMMIT;
