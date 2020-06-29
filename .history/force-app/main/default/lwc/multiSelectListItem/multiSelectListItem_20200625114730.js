616a0dbd3820c0243cd63abae53b848f2f9782f7
mshanemc <shane.mclaughlin@salesforce.com>
2020-06-29 15:45:05 -0500

    remove history folder

--- .history/force-app/main/default/lwc/multiSelectListItem/multiSelectListItem_20200625114730.js
+++
@@ -1,3 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class MultiSelectListItem extends LightningElement {}
--- .history/force-app/main/default/lwc/recommendationsMapModal/recommendationsMapModal_20200619110118.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement, api } from 'lwc';
-
-export default class RecommendationsMapModal extends LightningElement {
-    @api showmodal;
-
-    closeModal(){
-        this.showmodal = false;
-    }
-}
