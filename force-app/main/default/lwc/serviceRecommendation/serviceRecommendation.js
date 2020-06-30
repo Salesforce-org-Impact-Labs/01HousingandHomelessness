bd5bc081ffb563fefa483b85d8e86d4e92901e12
andrew-irwin <andrew.irwin@salesforce.com>
2020-06-30 17:24:44 -0500

    working print options

--- force-app/main/default/lwc/serviceRecommendation/serviceRecommendation.js
+++ force-app/main/default/lwc/serviceRecommendation/serviceRecommendation.js
@@ -31,6 +31,7 @@ export default class ServiceRecommendation extends LightningElement {
     @track newComment;
 
     connectedCallback(rec = this.servicerecommendation){
+        window.console.log(rec.Website);
         if(rec.ProgramType === 'Food'){
             this.programTypeFood = true
         } else if(rec.ProgramType === 'Education'){
